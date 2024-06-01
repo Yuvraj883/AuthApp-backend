317fa65fff8c1a7b5630fa85d364b0ed245e1a7c
Yuvraj <devyuvraj883@gmail.com>
2024-06-01 14:52:32 +0530

    Fixed errors

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const app= require('express')();
-const http = require('http').Server(app);
+const express = require('express');
+const http = require('http');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-// const app = express();
+const app = express();
 
 // Middleware
 app.use(bodyParser.json());
 app.use(cors());  // Enable CORS for all routes
 
 // Connect to MongoDB Atlas
-mongoose.connect("mongodb+srv://kingyuvi88:J6fyR8PyRkPTjEkA@cluster0.k0vl13e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect("mongodb+srv://kingyuvi88:J6fyR8PyRkPTjEkA@cluster0.k0vl13e.mongodb.net/yourDatabaseName?retryWrites=true&w=majority")
   .then(() => console.log('MongoDB Atlas connected'))
   .catch(err => console.log(err));
 
@@ -32,7 +32,7 @@ app.post('/api/users', async (req, res) => {
     // Send a success response
     res.status(201).json({ message: 'User created successfully', user: newUser });
   } catch (err) {
-    // Send an error response if something goes wrongg
+    // Send an error response if something goes wrong
     res.status(500).json({ error: err.message });
   }
 });
@@ -62,4 +62,4 @@ app.get('/api/users/:id', async (req, res) => {
 
 // Start the HTTP server
 const PORT = process.env.PORT || 5000;
-http.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+http.createServer(app).listen(PORT, () => console.log(`Server is running on port ${PORT}`));
